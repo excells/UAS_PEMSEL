29a7983d22076747dc7510c43dee2d3323c31786
Fadzillah Excel Aditya <59732605+excells@users.noreply.github.com>
2021-01-16 23:30:28 +0700

    Delete AppFootball directory

--- AppFootball/app/src/main/java/com/example/footballapp2/model/EventDetailResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.footballapp2.model
-
-/**
- * Created by Avin on 06/09/2018.
- */
-data class EventDetailResponse(
-        val events: List<EventDetail>
-)
--- AppFootball/app/src/main/java/com/example/footballapp2/model/EventSearchResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.footballapp2.model
-
-/**
- * Created by Avin on 21/09/2018.
- */
-data class EventSearchResponse(
-        val event: List<Event>
-)
